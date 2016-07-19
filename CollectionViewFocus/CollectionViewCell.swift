bf5bf4d6e41c78107be7b892d5430ff49acef1d0
Giovanny Aranda <garanda21@gmail.com>
2016-07-19 16:03:53 +0200

    Create all objects

---
+++ CollectionViewFocus/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  CollectionViewFocus
+//
+//  Created by Giovanny Aranda on 19/7/16.
+//  Copyright © 2016 Giovanny Aranda. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var lbCollectionName: UILabel!
+}
--- CollectionViewFocus/ViewController.swift
+++ CollectionViewFocus/ViewController.swift
@@ -8,18 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,  UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var btClick: UIButton!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    private var focusGuide: UIFocusGuide!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        collectionView.delegate = self;
+        collectionView.dataSource = self;
+        
+        focusGuide = UIFocusGuide();
+        focusGuide.preferredFocusedView = btClick
+        view.addLayoutGuide(focusGuide)
+        
+
+        focusGuide.topAnchor.constraintEqualToAnchor(collectionView.topAnchor).active = true
+        focusGuide.bottomAnchor.constraintEqualToAnchor(btClick.bottomAnchor).active = true
+        focusGuide.leadingAnchor.constraintEqualToAnchor(collectionView.leadingAnchor).active = true
+        focusGuide.widthAnchor.constraintEqualToAnchor(collectionView.widthAnchor).active = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewDidLayoutSubviews() {
+        self.view.showLayoutGuides()
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10;
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1;
+    }
 
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as! CollectionViewCell
+        cell.lbCollectionName.text = "Item \(indexPath.row+1)";
+        return cell;
+    }
 
 }
 
